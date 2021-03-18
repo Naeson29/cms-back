ee6e2f68e2270a780ce74a85b07069860b89e53e
Nicolas NAHMIAS <nnahmias@beable.fr>
2021-03-18 01:07:45 +0100

    Fix permission button

--- src/Containers/Functions/Default.js
+++ src/Containers/Functions/Default.js
@@ -2,7 +2,6 @@ export default (dispatch, creators, paramsList) => ({
     load: () => {
         if (creators) dispatch(creators.search.request(paramsList));
     },
-
     getMore: (page) => {
         if (creators) {
             dispatch(creators.more.request({
@@ -13,11 +12,9 @@ export default (dispatch, creators, paramsList) => ({
             }));
         }
     },
-
     getDetail: (id) => {
         if (creators) dispatch(creators.read.request(id));
     },
-
     destroy: (id) => {
         if (creators) dispatch(creators.destroy.request(id));
     },
