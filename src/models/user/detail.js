49abdcc48e664804518f9b6f21b9ad3e59e750b3
nnahmias <nicolas.nahmias@lemonsoft.fr>
2021-12-11 03:05:39 +0100

    user detail

--- src/models/user/detail.js
+++ src/models/user/detail.js
@@ -1,11 +1,20 @@
 
 import React from 'react';
+import moment from 'moment';
+import { getImage } from '../../utilities/functions';
 
 export default (key) => {
     const { first_name, last_name } = key;
     return (
-        <div>
-            <p>{`${first_name} ${last_name}`}</p>
+        <div className="detail-user">
+            <div className="image">
+                <img src={key.image ? getImage(key.image, 'medium') : './img/avatar.png'} alt="" />
+            </div>
+            <div className="infos">
+                <p className="name">{`${first_name} ${last_name}`}</p>
+                <p className="info created">{`Crée le ${moment(key.createdAt).format('D MMMM YYYY')}`}</p>
+                <p className="info email">{key.email}</p>
+            </div>
         </div>
     );
 };
