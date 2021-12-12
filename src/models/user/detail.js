d770bbe5349f9f88eeda47420872174fe4811e26
nnahmias <nicolas.nahmias@lemonsoft.fr>
2021-12-13 00:44:30 +0100

    Responsive list card

--- src/models/user/detail.js
+++ src/models/user/detail.js
@@ -4,16 +4,17 @@ import moment from 'moment';
 import { getImage } from '../../utilities/functions';
 
 export default (key) => {
-    const { first_name, last_name } = key;
+    const { first_name, last_name, role_name, email, createdAt, image = false } = key;
     return (
         <div className="detail-user">
             <div className="image">
-                <img src={key.image ? getImage(key.image, 'medium') : './img/avatar.png'} alt="" />
+                <img src={image ? getImage(key.image, 'medium') : './img/avatar.png'} alt="" />
             </div>
             <div className="infos">
                 <p className="name">{`${first_name} ${last_name}`}</p>
-                <p className="info created">{`Crée le ${moment(key.createdAt).format('D MMMM YYYY')}`}</p>
-                <p className="info email">{key.email}</p>
+                <p className="info created">{`Crée le ${moment(createdAt).format('D MMMM YYYY')}`}</p>
+                <p className="info email">{email}</p>
+                <p className="role">{role_name}</p>
             </div>
         </div>
     );
