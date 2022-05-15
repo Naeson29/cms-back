a15c435fbbe5af9aa23962db1cdd03eaf647fb86
nnahmias <nicolas.nahmias@lemonsoft.fr>
2022-05-15 15:54:23 +0200

    history push after create

--- src/sagas/functions.js
+++ src/sagas/functions.js
@@ -2,36 +2,34 @@ import {
     takeEvery, put, call,
 } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
 
 import {
     modalActions,
-    panelActions,
 } from '../actions';
 
 import toasts from './toast';
 
 const { defaultErrors, defaultSuccess, success } = toasts;
+const history = useHistory();
 
 const theme = 'colored';
 const paramToast = { theme };
 
-function* createSuccess(name) {
-    yield put(panelActions().creators.close.do());
+function* createSuccess(name, route) {
     yield call(() => toast.success(success[name] ? success[name].create : defaultSuccess.create, paramToast));
+    history.push(`/${route}`);
 }
 
 function* createFailure() {
-    yield put(panelActions().creators.close.do());
     yield call(() => toast.error(defaultErrors.create, paramToast));
 }
 
 function* updateSuccess(name) {
-    // yield put(panelActions().creators.close.do());
     yield call(() => toast.success(success[name] ? success[name].update : defaultSuccess.update, paramToast));
 }
 
 function* updateFailure() {
-    yield put(panelActions().creators.close.do());
     yield call(() => toast.error(defaultErrors.update, paramToast));
 }
 
@@ -47,10 +45,10 @@ function* destroyFailure() {
     yield call(() => toast.error(defaultErrors.delete, paramToast));
 }
 
-function* defaultRoot(name, types, defaultSagas) {
+function* defaultRoot(name, route, types, defaultSagas) {
     yield* defaultSagas.root();
     yield takeEvery(types.DESTROY.REQUEST, onDelete);
-    yield takeEvery(types.CREATE.SUCCESS, () => createSuccess(name));
+    yield takeEvery(types.CREATE.SUCCESS, () => createSuccess(name, route));
     yield takeEvery(types.UPDATE.SUCCESS, () => updateSuccess(name));
     yield takeEvery(types.DESTROY.SUCCESS, () => destroySuccess(name));
     yield takeEvery(types.CREATE.FAILURE, createFailure);
