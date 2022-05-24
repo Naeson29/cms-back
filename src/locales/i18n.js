f222b0adb96334b5a12aa6ead29bb717839a2e2e
nnahmias <nnahmias@outlook.fr>
2022-05-24 14:08:33 +0200

    english locals

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,11 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import fr from './fr';
+import en from './en';
 
 i18n
     .use(initReactI18next)
     .init({
-        resources: { fr },
+        resources: { fr, en },
         lng: 'fr',
         fallbackLng: 'fr',
         debug: true,
