8332b35c3a5cc23f67e0133b9532d48f408b739c
nnahmias <nicolas.nahmias@lemonsoft.fr>
2022-05-26 15:40:12 +0200

    fix lint

--- src/components/utilities/permission/permission.js
+++ src/components/utilities/permission/permission.js
@@ -1,6 +1,12 @@
-const getPermissionModel = (permissions, model, screen = null) => (
-    (permissions && permissions.data[model]) ? (!screen ? permissions.data[model] : permissions.data[model][screen]) : {}
-);
+const getPermissionModel = (permissions, model, screen = null) => {
+    let permissionsScreen = {};
+
+    if (permissions && permissions.data[model]) {
+        permissionsScreen = (!screen ? permissions.data[model] : permissions.data[model][screen]);
+    }
+
+    return permissionsScreen;
+};
 
 export default {
     getPermissionModel,
